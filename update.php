7154387f45cc8fb03436bd933800573787b50075
BlurplePanda <bootham@wgc.school.nz>
2022-07-01 09:26:24 +1200

    Copied in connection code

---
+++ update.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost", "bootham", "richpatch76", "bootham_cafe");
+if(mysqli_connect_errno()){
+    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+else{
+    echo "connected to database";
+}
+
