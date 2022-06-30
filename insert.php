1e27b54599700617f92285079e5f6fd4e0a53d1a
BlurplePanda <bootham@wgc.school.nz>
2022-07-01 09:01:42 +1200

    Cleaned up error messages

--- insert.php
+++ insert.php
@@ -5,19 +5,29 @@ if(mysqli_connect_errno()){
 else{
     echo "connected to database";
 }
+$alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 $drink = $_POST["drink_to_add"];
 $cost = $_POST["cost_to_add"];
+$id = strtoupper(substr($drink, 0, 1).substr($drink, -1, 1));
 
-$insert_drink = "INSERT INTO drinks (drinkName, Price) VALUES('$drink', '$cost')";
+while(true) {
+    $insert_drink = "INSERT INTO drinks (drinkID, drinkName, Price) VALUES('$id', '$drink', '$cost')";
 
-/* Check the data has been inserted */
-if(!mysqli_query($con, $insert_drink)) {
-    echo "Insert failed!";
-} else {
-    echo "Insert successful!";
+    /* Check the data has been inserted */
+    if (!mysqli_query($con, $insert_drink)) {
+        if (mysqli_errno($con) == 1062) {
+            echo "Insert failed, regenerating ID";
+            $id = substr($alphabet, rand(1, 26), 1).substr($alphabet, rand(1,26), 1);
+        } else {
+            echo "Insert failed!";
+            break;
+        }
+    } else {
+        echo "Insert successful!";
+        break;
+    }
 }
-
 /* Refresh the page after 2 seconds and return to the drinks.php page */
 header("refresh:2; url=drinks.php");
 
